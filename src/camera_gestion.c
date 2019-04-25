2f22bf1657a506fe497c24bb4e937b7877826143
Antoine Gosselin <agosselin@localhost.localdomain>
2019-04-25 13:49:39 +0200

    remove zoom

--- src/camera_gestion.c
+++ src/camera_gestion.c
@@ -7,21 +7,8 @@
 
 #include "rpg.h"
 
-static void zoom_gestion(rpg_t *rpg)
-{
-    if (CONTROLS.bools[ZOOM] == 1) {
-        sfView_zoom(WIND.view, 0.9);
-        CONTROLS.bools[ZOOM] = 0;
-    }
-    if (CONTROLS.bools[DEZOOM] == 1) {
-        sfView_zoom(WIND.view, 1.1);
-        CONTROLS.bools[DEZOOM] = 0;
-    }
-}
-
 void camera_control(rpg_t *rpg, sfVector2f pos, obj_t **obj)
 {
-    zoom_gestion(rpg);
     if (CONTROLS.bools[KEYY] == 0)
         sfView_setCenter(WIND.view, pos);
     else {
