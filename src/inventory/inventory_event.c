2191611760951541b7aefe90364683de3e721d55
guilian.ganster@epitech.eu <guilian.ganster@epitech.eu@epitech.eu>
2019-04-30 16:29:05 +0200

    solve drandrop bug

--- src/inventory/inventory_event.c
+++ src/inventory/inventory_event.c
@@ -26,10 +26,29 @@ void inventory_event_stuff(inventory_t *inv)
     }
 }
 
+static int is_under_dragging(inventory_t *inv)
+{
+    for (int i = 0; i < INVENTORY_SIZE; i++) {
+        if( inv->stock[i] == NULL)
+            continue;
+        if (dragndrop_isUnderDragging(inv->stock[i]->display))
+            return (TRUE);
+    }
+    for (int i = 0; i < 6; i++) {
+        if (inv->stuff[inv->state][i] == NULL)
+            continue;
+        if (dragndrop_isUnderDragging(inv->stuff[inv->state][i]->display))
+            return (TRUE);
+    }
+    return (FALSE);
+}
+
 void inventory_event_select(inventory_t *inv)
 {
     sfVector2i p = sfMouse_getPositionRenderWindow(inv->window);
 
+    if (is_under_dragging(inv))
+        return;
     if (!sfMouse_isButtonPressed(sfMouseLeft))
         return;
     if (p.x > 285 && p.x < 360 && p.y > 200 && p.y < 261)
