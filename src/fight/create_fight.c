8908025c5b922013218a76587b87a14b40cd8f9d
Kylian Balan <kylian.balan@epitech.eu>
2019-05-02 19:26:37 +0200

    update ennemy life

--- src/fight/create_fight.c
+++ src/fight/create_fight.c
@@ -42,8 +42,7 @@ static void init_fight_var(int i, sfVector2f vect, obj_t **obj, fight_t *new)
     new->old_pos[2] = sfSprite_getPosition(obj[3]->sprite);
     new->old_pos[3] = sfSprite_getPosition(obj[new->nb_fight + 6]->sprite);
     new->life[0] = obj[0]->stats[HPS] + obj[2]->stats[HPS] + obj[3]->stats[HPS];
-//    new->life[1] = obj[new->nb_fight + 6]->stats[HPS];
-    new->life[1] = 2;
+    new->life[1] = obj[new->nb_fight + 6]->stats[HPS];
     new->turn = 0;
     new->fight_status = 0;
 }
