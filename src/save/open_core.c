d910a9956f59e094ca7cd7904a12617bdb3e7a36
guilian.ganster@epitech.eu <guilian.ganster@epitech.eu@epitech.eu>
2019-05-03 11:53:07 +0200

    add killed status load

--- src/save/open_core.c
+++ src/save/open_core.c
@@ -7,9 +7,29 @@
 
 #include "save.h"
 
+static void killed_init_default(rpg_t *rpg)
+{
+    for (int i = 0; i < 3; i++)
+        rpg->killed[i] = 0;
+}
+
 static void restore_killed(rpg_t *rpg, ini_file_t *file)
 {
-    
+    ini_line_t *line = ini_file_get_from_key(file, "KILLED");
+    char *buff;
+
+    if (!line || line->values->size != 3) {
+        killed_init_default(rpg);
+        return;
+    }
+    for (int i = 0; i < 3; i++) {
+        buff = ini_line_get_value_from_col(line, i);
+        if (!my_strcmp(buff, "0") && !my_strcmp(buff, "1")) {
+            killed_init_default(rpg);
+            return;
+        }
+        rpg->killed[i] = buff[0] - '0';
+    }
 }
 
 void load(rpg_t *rpg, obj_t **objs)
